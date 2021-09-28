804fc44613aa4c835cdb52f3e2357752107be261
SarvarRahim <tesla_19@mail.ru>
2021-09-28 07:27:03 +0500

    add button copy

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const elForm = document.querySelector('.js-form');
 const elFormTexareaInput = elForm.querySelector('.js-input-textarea');
 const elFormTexareaOutput = elForm.querySelector('.js-output-textarea');
-const elRotate = elForm.querySelector(".js-filter");
-
-
+const elRotate = elForm.querySelector('.js-filter');
+const elClearBtn = elForm.querySelector('.js-btn-clear');
+const elCopyBtn = elForm.querySelector('.js-btn-copy');
 
 function rot (str,rot){
   let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -11,8 +11,9 @@ function rot (str,rot){
   for(let i = 0; i < str.length; i++){
       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase());
       let index = arrIndex + rot;
-      if(arrIndex == -1) result += str[i];
-      else if(str[i].toUpperCase() == str[i]){
+      if(arrIndex == -1){
+        result += str[i];
+      }else if(str[i].toUpperCase() == str[i]){
           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
       }else{
           result +=  index < 26 ? arr[index] : arr[index - arr.length];
@@ -29,8 +30,9 @@ function decrypt (str,rot){
   for(let i = 0; i < str.length; i++){
       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase());
       let index = arrIndex + rot;
-      if(arrIndex == -1) result += str[i]
-      else if(str[i].toUpperCase() == str[i]){
+      if(arrIndex == -1) {
+        result += str[i];
+      }else if(str[i].toUpperCase() == str[i]){
           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
       }else{
           result +=  index < 26 ? arr[index] : arr[index - arr.length];
@@ -51,62 +53,23 @@ evt.preventDefault();
 }
 elFormTexareaInput.addEventListener("keyup", doRot);
 
-function doDecrytp (evt) {
-  evt.preventDefault();
+function doDecrytp (ent) {
+  ent.preventDefault();
   elFormTexareaInput.value = doDecrytp(elFormTexareaOutput.value, Number(elRotate.value));
 }
 elFormTexareaOutput.addEventListener('keyup', doDecrytp);
 
+if (elClearBtn) {
+  elClearBtn.addEventListener('click', () => {
+    elFormTexareaInput.innerHTML = '';
+    elFormTexareaOutput.innerHTML = '';
+  })
+}
 
-// const elForm = document.querySelector('.js-form');
-// const elFormTexareaInput = elForm.querySelector('.js-input-textarea');
-// const elFormTexareaOutput = elForm.querySelector('.js-output-textarea');
-// const elRotate = elForm.querySelector('.js-filter');
-
-// function rot (str,rot){
-//   let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//   let result = '';
-//   for(let i = 0; i < str.length; i++){
-//       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase())
-//       let index = arrIndex + rot;
-//       if(arrIndex == -1) result += str[i];
-//       else if(str[i].toUpperCase() == str[i]){
-//           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
-//       }else{
-//           result +=  index < 26 ? arr[index] : arr[index - arr.length];
-//       }
-//   }
-//   return result;
-// }
-// console.log(rot('salom hello',13));
+elCopyBtn.addEventListener('click', () => {
+  elFormTexareaOutput.select();
+  document.execCommand('copy');
+})
 
 
 
-// function decrypt (str,rot){
-//   let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].reverse('');
-//   let result = '';
-//   for(let i = 0; i < str.length; i++){
-//       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase());
-//       let index = arrIndex + rot;
-//       if(arrIndex == -1) result += str[i];
-//       else if(str[i].toUpperCase() == str[i]){
-//           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
-//       }else{
-//           result +=  index < 26 ? arr[index] : arr[index - arr.length];
-//       }
-//   }
-//   return result;
-// }
-// console.log(decrypt('fnybz uryyb',13));
-
-// function doRot(evt){
-//   evt.preventDefault();
-//     elFormTexareaOutput.value = rot(elFormTexareaInput.value, Number(elRotate.value));
-//   }
-//   elFormTexareaInput.addEventListener('keyup', rot);
-
-//   function doDecrytp (evt) {
-//     evt.preventDefault();
-//     elFormTexareaInput.value = decrypt(elFormTexareaOutput.value, Number(elRotate.value));
-//   }
-//   elFormTexareaOutput.addEventListener('keyup', decrypt);
