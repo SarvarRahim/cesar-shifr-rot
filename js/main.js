fe00e6eb861575ebe1a837cb74868e8947ed47cf
SarvarRahim <tesla_19@mail.ru>
2021-09-28 06:45:22 +0500

    all done js code

--- js/main.js
+++ js/main.js
@@ -47,7 +47,66 @@ elForm.addEventListener('submit', evt => {
 
 function doRot(evt){
 evt.preventDefault();
-  elFormTexareaOutput.value = rot(elFormTexareaInput.value, Number(elRotate.value))
+  elFormTexareaOutput.value = rot(elFormTexareaInput.value, Number(elRotate.value));
 }
+elFormTexareaInput.addEventListener("keyup", doRot);
 
-elFormTexareaInput.addEventListener("keyup", doRot)
+function doDecrytp (evt) {
+  evt.preventDefault();
+  elFormTexareaInput.value = doDecrytp(elFormTexareaOutput.value, Number(elRotate.value));
+}
+elFormTexareaOutput.addEventListener('keyup', doDecrytp);
+
+
+// const elForm = document.querySelector('.js-form');
+// const elFormTexareaInput = elForm.querySelector('.js-input-textarea');
+// const elFormTexareaOutput = elForm.querySelector('.js-output-textarea');
+// const elRotate = elForm.querySelector('.js-filter');
+
+// function rot (str,rot){
+//   let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//   let result = '';
+//   for(let i = 0; i < str.length; i++){
+//       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase())
+//       let index = arrIndex + rot;
+//       if(arrIndex == -1) result += str[i];
+//       else if(str[i].toUpperCase() == str[i]){
+//           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
+//       }else{
+//           result +=  index < 26 ? arr[index] : arr[index - arr.length];
+//       }
+//   }
+//   return result;
+// }
+// console.log(rot('salom hello',13));
+
+
+
+// function decrypt (str,rot){
+//   let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].reverse('');
+//   let result = '';
+//   for(let i = 0; i < str.length; i++){
+//       let arrIndex = arr.findIndex((ind) => ind == str[i].toLowerCase());
+//       let index = arrIndex + rot;
+//       if(arrIndex == -1) result += str[i];
+//       else if(str[i].toUpperCase() == str[i]){
+//           result +=  index < 26 ? arr[index].toUpperCase() : arr[index - arr.length].toUpperCase();
+//       }else{
+//           result +=  index < 26 ? arr[index] : arr[index - arr.length];
+//       }
+//   }
+//   return result;
+// }
+// console.log(decrypt('fnybz uryyb',13));
+
+// function doRot(evt){
+//   evt.preventDefault();
+//     elFormTexareaOutput.value = rot(elFormTexareaInput.value, Number(elRotate.value));
+//   }
+//   elFormTexareaInput.addEventListener('keyup', rot);
+
+//   function doDecrytp (evt) {
+//     evt.preventDefault();
+//     elFormTexareaInput.value = decrypt(elFormTexareaOutput.value, Number(elRotate.value));
+//   }
+//   elFormTexareaOutput.addEventListener('keyup', decrypt);
